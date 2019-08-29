55d8503dbf90f5782880543c95ea361c6ecb0f68
Victoria <vyoung708@yahoo.com>
2019-08-29 11:33:41 -0400

    Add files via upload

---
+++ databaseClass.js
@@ -0,0 +1,37 @@
+'use strict'
+
+var EventEmitter = require('events').EventEmitter;
+var mysql = require('mysql');
+
+var dbinfo = require('../Passwords/databaseinfo.json');
+
+var con = mysql.createConnection(dbinfo);
+con.connect(function(err) {
+	if (err) {
+		console.log('Error connecting to database');
+	} else {
+		console.log('Database successfully connected');
+	}
+});
+
+class Database extends EventEmitter {
+	constructor(){super();}
+	login(username, password) {
+		var str = "SELECT type FROM accounts WHERE username=" + con.escape(username) + " AND password=PASSWORD(" + con.escape(password) + ")";
+		var self = this;
+		con.query(str,
+			function(err, rows, fields) {
+				if(err) {
+					console.log('Error');
+					return 0;
+				} else {
+					if(rows.length>0)
+						self.emit('loggedin',1);
+					else
+						self.emit('loggedin',0);
+				}
+			}
+		);
+	}
+}
+exports.Database = Database
---
+++ login.js
@@ -0,0 +1,50 @@
+var database = require('./database');
+var db = new database();
+
+var express = require('express');
+var app = express();
+var session = require('client-sessions');
+app.use(session({
+	cookieName: 'session',
+	secret: 'edwardallenpoe1234',
+	duration: 30*60*1000,
+	activeDuration: 5*60*1000,
+}));
+
+app.get('/', function (req, res){
+	res.write('<html><body>');
+	if(req.session.msg){
+		res.write(req.session.msg);
+		delete req.session.msg;
+	}
+	req.session.destroy();
+	res.write('<html>
+		<body>
+		<form method=post action='/login'>
+		<input type=text name=username>
+		<input type=password name=password>
+		<input type=submit value=Login>
+		</form>
+		</body>
+		</html>');
+	res.end();
+});
+
+app.post('/login',
+	db.once('loggedin', function(msg) {
+		if(msg==1) {
+			req.session.userid=req.body.username;
+			return res.redirect('/<landing page>');
+		} else {
+			req.session.msg = "Invalid login";
+			return res.redirect('/');
+		}
+	});
+	db.login(req.body.username, req.body.password);
+});
+
+app.get('/logout', function(req, res) {
+	req.session.reset();
+	req.session.msg = 'You logged out';
+	return res.redirect('/');
+});
